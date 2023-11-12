df7993da8131067b8775dfd39b9bd1336b16f3f5
Rizwan Shaikh <119811242+imrzn@users.noreply.github.com>
2023-11-12 16:34:54 +0530

    Update google_sheets.rb

--- _plugins/google_sheets.rb
+++ _plugins/google_sheets.rb
@@ -1,5 +1,27 @@
+require 'openssl'
+require 'base64'
 require 'net/http'
-api_key = 'AIzaSyCl1SKJlzGSHC2eG6qXUI9hjJagZ-eAVFU'
+
+def decrypt_password(encrypted_password, key, iv)
+  decipher = OpenSSL::Cipher.new('AES-256-CBC')
+  decipher.decrypt
+  decipher.key = key
+  decipher.iv = iv
+
+  decrypted = decipher.update(encrypted_password) + decipher.final
+end
+
+def read_from_file(filename)
+  File.read(filename)
+end
+
+encrypted_password = read_from_file('_plugins/xyz/1.txt')
+key = read_from_file('_plugins/xyz/2.txt')
+iv = read_from_file('_plugins/xyz/3.txt')
+
+decrypted_password = decrypt_password(encrypted_password, key, iv)
+
+api_key = Base64.decode64(decrypted_password)
 def fetch_sheet_data(api_key, spreadsheet_id, sheet_name)
   range = "#{sheet_name}"
   url = URI.parse("https://sheets.googleapis.com/v4/spreadsheets/#{spreadsheet_id}/values/#{range}?key=#{api_key}")
